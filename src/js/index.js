0f6cd9d2b4f0f803f095ad1f61040c6427ca7d8e
Felipe Fontelas <felipefontelas.123@gmail.com>
2020-03-04 15:53:29 -0300

    parte de produtos começar a puxar json

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-console.log("Hello Webpack");
+$(document).ready(function() {
+  $(".banners").slick({
+    dots: true,
+    arrows: false
+  });
+
+  $(".sold-itens").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      "<img class='a-left control-c prev slick-prev' src='./../public/img/back.png'>",
+    nextArrow:
+      "<img class='a-right control-c next slick-next' src='./../public/img/next.png'>"
+  });
+
+  $.getJSON(
+    "https://www.clickqi.com.br/api/dataentities/CG/search?_fields=productName,productRating,productListPrice,productBestPrice,productInstallments,productInstallmentsValue,productImage&_sort=productName%20DESC",
+    function(data) {
+      console.log(data);
+    }
+  );
+});
